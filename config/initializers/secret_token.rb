71e0b0d8391b3f3f7ed8fdfa9f5292f7e79e8dff
Osama Hussain <sohussain@gmail.com>
2012-09-30 18:39:47 +0500

    initial bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monitore::Application.config.secret_token = '343284e212ee94ca951dcefd00db8f2a4bc8557578033cb9a935e20a8a97ab0689e69b3a994477c606ddb38d89d12ebff6b7f398b7dfc96da53aae2491dbb0eb'
